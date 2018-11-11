f095e101f28b72f9d8d3e32ac301f1c068c9dec7
Администратор <@AKARAHOME>
2018-11-11 18:45:21 +0700

    create classes: Manufacture, Workshop, Product

---
+++ OOP2/Manufacture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    abstract class Manufacture:Object
+    {
+        public string WorkshopName { get; set; }
+        public string WorkshopChief { get; set; }
+        public int WorkerAmount { get; set; }
+
+        //Abstract method, without implementation
+        public abstract void Some();
+
+        //Virtual
+        public virtual void GetWorkshop()
+        {
+            Console.WriteLine($"Workshop: { WorkshopName }, Chief: {WorkshopChief} ");
+        }
+
+        protected Manufacture( string WorkshopName, string WorkshopChief, int Workermount)
+        {
+            this.WorkshopName = WorkshopName;
+            this.WorkshopChief = WorkshopChief;
+            this.WorkerAmount = WorkerAmount;
+        }
+  
+    }
+}
---
+++ OOP2/Workshop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    class Workshop : Manufacture
+    {
+        public Workshop(string WorkshopName, string WorkshopChief, int WorkshopAmount)
+            : base (WorkshopName, WorkshopChief, WorkshopAmount)
+        {
+
+        }
+
+        public override void Some()
+        {
+            Product production = new Product();
+        }
+
+    }
+}
